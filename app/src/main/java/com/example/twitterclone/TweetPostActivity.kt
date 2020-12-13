d27c2d0565ca72502f5a301b56587e78ef92ed34
Manish Panwar <panwarmanish843@gmail.com>
2020-12-13 23:16:09 +0530

    Added PostTweetActivity

---
+++ app/src/main/java/com/example/twitterclone/TweetPostActivity.kt
@@ -0,0 +1,12 @@
+package com.example.twitterclone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class TweetPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tweet_post)
+
+    }
+}
